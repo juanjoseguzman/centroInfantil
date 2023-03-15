e78660ed7505525db37a7cb8485447c8a9ececb0
Franm0rales <PICASSOMORALES@GMAIL.COM>
2023-03-15 13:42:38 +0100

    comit

--- src/components/Datos/Datos.jsx
+++ src/components/Datos/Datos.jsx
@@ -1,18 +1,25 @@
 import bg2 from "../../../public/Imagenes/bg-2.jpg";
 import ico4 from "../../../public/Imagenes/ico-4.png";
-
+import "./Datos.css"
 export function Datos() {
   return (
     <div className="mt-5 pt-5">
       <img className="img-fluid" src={bg2} alt="fondo" />
       <div className="container">
-        <div className="contenedor">
+        <div className="contenedor d-flex">
           <div className="col-1">
-            <img className="texto" src={ico4} />
+            <img className="img-container"  src={ico4} />
           </div>
-          <div>
-            <img className="texto" src={ico4} />
+          <div className="col-1">
+            <img className="img-container"  src={ico4} />
           </div>
+          <div className="col-1">
+            <img className="img-container"  src={ico4} />
+          </div>
+          <div className="col-1">
+            <img className="img-container"  src={ico4} />
+          </div>
+         
         </div>
       </div>
     </div>
