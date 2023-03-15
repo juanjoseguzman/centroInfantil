0304ba60cad26cbbacc47300f5b9c0c689a3e224
Franm0rales <PICASSOMORALES@GMAIL.COM>
2023-03-15 09:53:02 +0100

    css botones

--- src/components/BotonesHome/BotonesHome.jsx
+++ src/components/BotonesHome/BotonesHome.jsx
@@ -5,7 +5,7 @@ export function BotonesHome({ texto, clase, icono }) {
   return (
     <div className="d-flex ">
       <Link
-        className={`${clase} gap-2 p-3 border-0 text-decoration-none text-white custom-rounded efectoHover`}
+        className={`${clase} gap-2 p-3 border-0 text-decoration-none text-white btn-rebote custom-rounded `}
       >
         <img src={icono} /> {texto}
       </Link>
