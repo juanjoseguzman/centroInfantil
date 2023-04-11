4985e3f30cf1a21edcb1efacbffe9e6d53bda1b8
Franm0rales <PICASSOMORALES@GMAIL.COM>
2023-04-11 18:14:49 +0200

    ofrecemos

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 
 import { Home } from "./views/Home/Home";
+import QueOfrecemos from "./views/QueOfrecemos/QueOfrecemos";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
+          <Route path="ofrecemos" element={<QueOfrecemos/>}/>
         </Route>
       </Routes>
     </BrowserRouter>
